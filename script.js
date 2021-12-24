a4546da2d959c061fa94c08e7f11ee15521e6c5d
kotesh_Mudila <69259490+kotesh-arya@users.noreply.github.com>
2021-12-24 16:43:37 +0000

    Added counter app

--- script.js
+++ script.js
@@ -647,9 +647,27 @@ sample.forEach(function (item, index, array) {
  console.log(index, index);
 });
 
+// ---------------------------------------------------------------------------
 
+// Added counter app
 
+let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContenttStr
+    countEl.textContent = 0
+    count = 0
+}
+
+console.log("Let's count people on the subway!")
 
 
 
