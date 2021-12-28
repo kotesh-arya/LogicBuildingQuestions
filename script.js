77cde1b56b6f67bdf9775ee588a21e0546880b4a
kotesh_Mudila <69259490+kotesh-arya@users.noreply.github.com>
2021-12-28 10:02:18 +0000

    FEAT: Updated Fruits in self logic

--- script.js
+++ script.js
@@ -671,70 +671,94 @@ sample.forEach(function (item, index, array) {
 
 --------------------------------------------------------------------
 
-//The black jack Game app
-let player = {
-    name: "Kotesh",
-    chips: 200
-}
+// //The black jack Game app
 
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
-let playerEl = document.getElementById("player-el")
 
-playerEl.textContent = player.name + ": $" + player.chips
+// let player = {
+//     name: "Kotesh",
+//     chips: 200
+// }
 
-function getRandomCard() {
-    let randomNumber = Math.floor( Math.random()*13 ) + 1
-    if (randomNumber > 10) {
-        return 10
-    } else if (randomNumber === 1) {
-        return 11
-    } else {
-        return randomNumber
-    }
-}
+// let cards = []
+// let sum = 0
+// let hasBlackJack = false
+// let isAlive = false
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+// let cardsEl = document.getElementById("cards-el")
+// let playerEl = document.getElementById("player-el")
 
-function startGame() {
-    isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    renderGame()
-}
+// playerEl.textContent = player.name + ": $" + player.chips
 
-function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
-    }
+// function getRandomCard() {
+//     let randomNumber = Math.floor( Math.random()*13 ) + 1
+//     if (randomNumber > 10) {
+//         return 10
+//     } else if (randomNumber === 1) {
+//         return 11
+//     } else {
+//         return randomNumber
+//     }
+// }
+
+// function startGame() {
+//     isAlive = true
+//     let firstCard = getRandomCard()
+//     let secondCard = getRandomCard()
+//     cards = [firstCard, secondCard]
+//     sum = firstCard + secondCard
+//     renderGame()
+// }
+
+// function renderGame() {
+//     cardsEl.textContent = "Cards: "
+//     for (let i = 0; i < cards.length; i++) {
+//         cardsEl.textContent += cards[i] + " "
+//     }
     
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
+//     sumEl.textContent = "Sum: " + sum
+//     if (sum <= 20) {
+//         message = "Do you want to draw a new card?"
+//     } else if (sum === 21) {
+//         message = "You've got Blackjack!"
+//         hasBlackJack = true
+//     } else {
+//         message = "You're out of the game!"
+//         isAlive = false
+//     }
+//     messageEl.textContent = message
+// }
+
+
+// function newCard() {
+//     if (isAlive === true && hasBlackJack === false) {
+//         let card = getRandomCard()
+//         sum += card
+//         cards.push(card)
+//         renderGame()        
+//     }
+// }
+
+// ---------------------------------------------------------------
+
+// Fruits in shelf logic
+
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+
+function arrangeFruits(){
+    for(let i=0;i<fruit.length;i++){
+        if(fruit[i] === "🍎" ){
+            appleShelf.textContent += "🍎"
+        }else{
+            orangeShelf.textContent += "🍊"
+        }
     }
-    messageEl.textContent = message
 }
-
-
-function newCard() {
-    if (isAlive === true && hasBlackJack === false) {
-        let card = getRandomCard()
-        sum += card
-        cards.push(card)
-        renderGame()        
-    }
-}
-
+arrangeFruits();
